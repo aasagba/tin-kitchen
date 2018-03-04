05d273600f88fa079ce07a0cf4fdbf8d50d05848
Adrian Asagba <adrian.asagba@ctt.co.uk>
2018-03-04 19:27:13 +0000

    centre aligned nav, welcome title social icons and meet section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -22,7 +21,7 @@ import { EventsComponent } from './events/events.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
